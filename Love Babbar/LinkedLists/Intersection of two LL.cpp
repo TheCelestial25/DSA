2a6c1084748ca7344f30bd8391ef5179cf33a490
Priyanshu Singh <37958393+TheCelestial25@users.noreply.github.com>
2021-12-27 19:37:27 +0530

    Create Intersection of two LL.cpp

---
+++ Love Babbar/LinkedLists/Intersection of two LL.cpp
@@ -0,0 +1,12 @@
+int intersectPoint(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* p1 = head1, * p2 = head2;
+    while(p1 != p2) {
+        if(p1 == NULL) p1 = head2;
+        if(p2 == NULL) p2 = head1;
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+    return p1->data;
+}
